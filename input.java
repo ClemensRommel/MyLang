1519288f2c3f9dfaf9302ba2765b4b29a4cd7681
ClemensRommel <clemens.rommel@mlbx.de>
2023-11-24 15:56:20 +0100

    Listensupport teil 1

--- input.java
+++ input.java
@@ -1,22 +1,16 @@
 import static std.codegen.Functions.*;
 import std.codegen.*;
 
+import java.util.ArrayList;
+
 class Main {
-    public static class Test {
-        public double x;
-        
-        public double test()  {
-            return x;
-        }
-        public Test() {
-            x = 3;
-        }
-    }
     public static void main() {
-        final Test test;
-        test = new Test();
-        test.x = 4;
-        System.out.println(test.test());
+        final ArrayList<Double> test;
+         test = new ArrayList<>(3);
+        test.add(1.0);
+        test.add(2.0);
+        test.add(3.0);
+        System.out.println(test);
     }
     
     public static void main(String[] args) {
