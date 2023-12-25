6198e1c5cb20b59dd83f352545cd6685293697fe
= <=>
2023-12-25 12:55:03 +0100

    While loops

--- input.java
+++ input.java
@@ -4,48 +4,31 @@ import std.runtime.*;
 import java.util.ArrayList;
 
 class Main {
-    public static double A(double k, __a0 x1, __a0 x2, __a0 x3, __a0 x4, __a0 x5) {
-        NumberUpvalue m = new NumberUpvalue();
-        m.value = k;
-        final Upvalue<__a0>B = new Upvalue<>();
-        B.value = () -> {
-            m.value = m.value - 1.0;
-            return A(m.value, B.value, x1, x2, x3, x4);
-        }
-        ;
-        if((k <= 0.0)) {
-            var __a1 = x4.call();
-            var __a2 = x5.call();
-            return __a1 + __a2;
-        } else {
-            return B.value.call();
-        }
-    }
     public static void main() {
-        final __a0 __a3 = () -> {
-            return 1.0;
-        };
-        final __a0 __a4 = () -> {
-            return -1.0;
-        };
-        final __a0 __a5 = () -> {
-            return -1.0;
-        };
-        final __a0 __a6 = () -> {
-            return 1.0;
-        };
-        final __a0 __a7 = () -> {
-            return 0.0;
-        };
-        var __a8 = A(10.0, __a3, __a4, __a5, __a6, __a7);
-        System.out.println(__a8);
+        final ArrayList<Double> lst;
+        lst = new ArrayList<>(3);
+        lst.add(1.0);
+        lst.add(2.0);
+        lst.add(3.0);
+        final ArrayList<Double> results;
+        final ArrayList<Double> __a1;
+        __a1 = new ArrayList<>();
+        for(var __a0 : lst) {
+            final double x;
+            x = __a0;
+            final double __a2;
+            {
+                __a2 = x;
+            }
+            __a1.add(__a2);
+            
+        }
+        results = __a1;
+        System.out.println(results);
     }
     
     public static void main(String[] args) {
         main();
-    }
-    @FunctionalInterface static interface __a0 {
-        double call();
     }
     
 }
