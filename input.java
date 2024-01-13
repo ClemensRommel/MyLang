d217760c465a46b41b2633fd001cdcb8b04eb65d
= <=>
2024-01-13 10:32:52 +0100

    Bugfixes für Analyze.myl + Range-Expression + erster tiel von enums

--- input.java
+++ input.java
@@ -4,31 +4,114 @@ import std.runtime.*;
 import java.util.ArrayList;
 
 class Main {
+    public static double f(double x)  {
+        return x * 2.0;
+    }
+    public static class Test {
+        public final double test;
+        {
+            test = 3.0;
+        }
+        public Test getTest()  {
+            return this;
+        }
+    }
     public static void main() {
-        final ArrayList<Double> lst;
-        lst = new ArrayList<>(3);
-        lst.add(1.0);
-        lst.add(2.0);
-        lst.add(3.0);
-        final ArrayList<Double> results;
-        final ArrayList<Double> __a1;
-        __a1 = new ArrayList<>();
-        for(var __a0 : lst) {
-            final double x;
-            x = __a0;
-            final double __a2;
+        System.out.println(1.0);
+        System.out.println("test");
+        System.out.println(true);
+        System.out.println("null");
+        double x;
+        x = 1.0;
+        System.out.println(x);
+        System.out.println((x + 1.0));
+        System.out.println((-x));
+        var __a0 = f(x);
+        System.out.println(__a0);
+        final __a2 __a1 = (double y) -> {
+            return 3.0 * y;
+        };
+        var __a3 = __a1.call(x);
+        System.out.println(__a3);
+        final double __a4;
+        if(true) {
+            __a4 = x;
+        } else {
+            __a4 = 3.0;
+        }
+        System.out.println(__a4);
+        ArrayList<Double> __a5 = new ArrayList<>(3);
+        __a5.add(1.0);
+        __a5.add(2.0);
+        __a5.add(3.0);
+        System.out.println(__a5);
+        System.out.println(__range(1.0, 3.0));
+        ArrayList<Double> __a6 = new ArrayList<>(3);
+        __a6.add(1.0);
+        __a6.add(2.0);
+        __a6.add(3.0);
+        System.out.println(__a6.get(__toIndex(1.0)));
+        var __a7 = new Test();
+        System.out.println(__a7.test);
+        final __a8 __a9;
+        __a9 = new __a8(1.0, 2.0);
+        System.out.println(__a9);
+        final String __a10;
+        {
+            System.out.println("hello ");
+            __a10 = "world";
+        }
+        System.out.println(__a10);
+        final ArrayList<Double> __a11;
+        __a11 = new ArrayList<>();
+        while(true) {
+            if(!(x < 5.0)) { break; }
+            final double __a12;
             {
-                __a2 = x;
+                x = x + 1.0;
+                __a12 = x;
             }
-            __a1.add(__a2);
+            __a11.add(__a12);
             
         }
-        results = __a1;
-        System.out.println(results);
+        System.out.println(__a11);
+        final ArrayList<Double> __a14;
+        __a14 = new ArrayList<>();
+        final double __a15;__a15 = 1.0;
+        final double __a16;__a16 = 4.0;
+        if(__a15 < __a16) 
+        for(double __a13 = __a15; __a13 < __a16; __a13++) {
+            final double y;
+            y = __a13;
+            final double __a17;
+            {
+                __a17 = y + 1.0;
+            }
+            __a14.add(__a17);
+            
+        }
+        System.out.println(__a14);
+        var __a18 = new Test();
+        var __a19 = __a18.getTest();
+        System.out.println(__a19);
     }
     
     public static void main(String[] args) {
         main();
+    }
+    static record __a8(double field0, double field1) {
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("(");
+            builder.append(field0);
+            builder.append(", ");
+            builder.append(field1);
+            builder.append(")");
+            return builder.toString();
+        }
+    }
+    @FunctionalInterface static interface __a2 {
+        double call(double __a20);
     }
     
 }
--- std/runtime/Functions.java
+++ std/runtime/Functions.java
@@ -1,5 +1,7 @@
 package std.runtime;
 
+import java.util.ArrayList;
+
 public class Functions {
     // Ignore a value. Used for expression statements and main()
     public static <A> void __ignore(A a) {
@@ -12,5 +14,13 @@ public class Functions {
         } else {
             throw new InvalidIndexException(d);
         }
+    }
+    public static ArrayList<Double> __range(double start, double end) {
+        ArrayList<Double> lst = new ArrayList<>();
+        if(end < start) return lst;
+        for (double i = start; i < end; i++) {
+            lst.add(i);
+        }
+        return lst;
     }
 }
